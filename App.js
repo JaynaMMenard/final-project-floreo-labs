9c269d0c2a0e3b17909ed473fd1ae83883062052
Yafira <yafira.martinez@lc.cuny.edu>
2020-01-23 23:21:22 -0500

    Added main app file -> App.js

---
+++ App.js
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import Game from "./components/Game/Game";
+import GameOver from "./components/GameOver/GameOver";
+import "./App.css";
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: "active",
+      winner: null,
+      whiteScore: 0,
+      blackScore: 0,
+      intro: true
+    };
+  }
+  
+  render() {
+    const startGame = () => {
+      this.setState({ intro: false });
+    };
+    if (this.state.intro) {
+      return (
+        <div className="intro">
+          <div className="intro-container">
+            <h1> Othello</h1>
+            <button id="game" onClick={startGame}>
+              {" "}
+              PLAY{" "}
+            </button>
+          </div>
+          
+        </div>
+      );
+    }
+
+
+    let game =
+      this.state.status === "active" ? (
+        <Game end={this.endGame.bind(this)} />
+      ) : (
+        ""
+      );
+      
+    let gameOver =
+      this.state.status === "over" ? (
+        <GameOver
+          winner={this.state.winner}
+          restart={this.restartGame.bind(this)}
+          white={this.state.whiteScore}
+          black={this.state.blackScore}
+        />
+      ) : ("");
+
+      const infobtnClicked = () => {
+        document.querySelector('.modal').style.display = "block";
+      }
+
+      const closebtnClicked = () => {
+        document.querySelector('.modal').style.display = "none";
+      }
+
+      // Music playing...
+    return (
+      <div className="App">
+        <audio
+          src="/audioclips/Vivaldi.mp3"
+          controls
+          autoPlay
+          hidden
+          className="music"
+        />
+        {game}
+
+        
+    );
+
+    
+  }
+
+  
+
+  restartGame() {
+    this.setState({
+      status: "active"
+    });
+  }
+  endGame(winner, whiteScore, blackScore) {
+    this.setState({
+      status: "over",
+      winner,
+      whiteScore,
+      blackScore
+    });
+  }
+}
+export default App;
