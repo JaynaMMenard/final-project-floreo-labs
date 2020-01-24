67a4c17c3bd96e4b3ba693db423029ec2fe40985
Yafira <yafira.martinez@lc.cuny.edu>
2020-01-24 11:02:27 -0500

    Updated modal window with instructions

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ class App extends Component {
       intro: true
     };
   }
-
+  
   render() {
     const startGame = () => {
       this.setState({ intro: false });
@@ -30,7 +30,7 @@ class App extends Component {
               PLAY{" "}
             </button>
           </div>
-
+          
         </div>
       );
     }
@@ -42,7 +42,7 @@ class App extends Component {
       ) : (
         ""
       );
-
+      
     let gameOver =
       this.state.status === "over" ? (
         <GameOver
@@ -76,25 +76,32 @@ class App extends Component {
         {/* Modal Pop-up Window */}
 
         <button className='infobtn' onClick={infobtnClicked}>How to Play</button>
-
-
+  
+        
         <div className='modal'>
           <div className='modal-content'>
             <button class="close" onClick={closebtnClicked}>&times;</button>
-            <p>Instructions</p>
+            <p><h3><b>Start of the game</b></h3>The game is started in the position shown below on a board consisting of 64 squares in an 8x8 grid.</p>
+            <p><h3><b>Basic rules</b></h3>Each reversi disk has a black side and a white side. On your turn, you place one piece on the board with your color facing up. You must place the piece so that an opponent's piece, or a row of opponent's pieces, is flanked by your pieces. All of the opponent's pieces between your pieces are then turned over to become your color.</p>
+            <p><h3><b>Aim of the game</b></h3>The object of the game is to own more pieces than your opponent when the game is over. The game is over when neither player has a move. Usually, this means the board is full. </p>
+            <p><h3><b>Playing the game</b></h3>A move consists of placing one piece on an empty square.</p>
+            <p><h4><b>Capture:</b></h4>You can capture vertical, horizontal, and diagonal rows of pieces. Also, you can capture more than one row at once.</p>
+            <p><h3><b>End of the game</b></h3>The game ends when:
+            <p>•One player wins, by making his color dominant on the board.</p>
+	          <p>•The players agree to finish the game (as a resignation, or a draw).</p>
+	          
+            </p>
           </div>
         </div>
 
         {gameOver}
       </div>
-
-
     );
 
-
+    
   }
 
-
+  
 
   restartGame() {
     this.setState({
